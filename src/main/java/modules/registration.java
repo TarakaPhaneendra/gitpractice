9a90c07a79cfe09cb9f0c71368cb1862e82a1c93
TarakaPhaneendra <1234taraka@gmail.com>
2025-10-19 14:07:53 +0530

    WIP: changes before merging feature

--- src/main/java/modules/registration.java
+++ src/main/java/modules/registration.java
@@ -5,7 +5,7 @@ public class registration {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("registration module completed");
-System.out.println("fskfkfjsdkfjdlkfjskfdjfksdjfksjfds");
+
 	}
 
 }
