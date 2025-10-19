f43c29a74d1c192742434003101137e8990a4b0c
TarakaPhaneendra <1234taraka@gmail.com>
2025-10-19 11:40:07 +0530

    registration module successful

---
+++ src/main/java/modules/registration.java
@@ -0,0 +1,10 @@
+package modules;
+
+public class registration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("login module completed");
+	}
+
+}
