a5d236a55180013cac02c9f0e6b1b934d86ce3d9
TarakaPhaneendra <1234taraka@gmail.com>
2025-10-19 12:58:48 +0530

    merge feature to master

---
+++ src/main/java/modules/registration.java
@@ -0,0 +1,11 @@
+package modules;
+
+public class registration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("registration module completed");
+System.out.println("fskfkfjsdkfjdlkfjskfdjfksdjfksjfds");
+	}
+
+}
